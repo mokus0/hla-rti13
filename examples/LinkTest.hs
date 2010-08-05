2f6926d5c45850acdd5fc86c00514e4a0fe4e66d
James Cook <james.cook@usma.edu>
2010-08-05 18:43:11 +0000

    updated the build instructions for LinkTest.hs

--- examples/LinkTest.hs
+++ examples/LinkTest.hs
@@ -4,7 +4,8 @@ import Network.HLA.RTI13
 import Network.HLA.RTI13.RTITypes.FFI
 
 -- simple test to ensure dynamic library gets loaded and linked in properly
--- build with: -optl-L$RTI_HOME/$RTI_BUILD_TYPE/lib
+-- build with "ghc --make", the hla-rti13 package's package description
+-- should include info about where to find the libraries needed.
 main = do
     putStrLn ("testing constants: " ++ show [rtiVersion, defaultSpaceName, show wrap_RTI_MAJOR_VERSION])
     rti_ambassador <- getRTIAmbassador :: IO (RTIAmbassador ())
