582b10f25f8d2e9d16611a7000c891f015e1af49
James Cook <james.cook@usma.edu>
2010-08-06 15:41:52 +0000

    commented out references to FederateAmbServices in RTI13.hs

--- src/Network/HLA/RTI13.hs
+++ src/Network/HLA/RTI13.hs
@@ -1,6 +1,6 @@
 module Network.HLA.RTI13 
     ( module Network.HLA.RTI13.BaseTypes
-    , module Network.HLA.RTI13.FederateAmbServices
+--    , module Network.HLA.RTI13.FederateAmbServices
     , module Network.HLA.RTI13.FedTime
     , module Network.HLA.RTI13.HsFederateAmbassador
     , module Network.HLA.RTI13.NullFederateAmbassador
@@ -10,7 +10,7 @@ module Network.HLA.RTI13
     ) where
 
 import Network.HLA.RTI13.BaseTypes
-import Network.HLA.RTI13.FederateAmbServices
+-- import Network.HLA.RTI13.FederateAmbServices
 import Network.HLA.RTI13.FedTime
 import Network.HLA.RTI13.HsFederateAmbassador
 import Network.HLA.RTI13.NullFederateAmbassador
