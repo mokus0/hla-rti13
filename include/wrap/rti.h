d656bdbbfe657df4c895f1df72c696fcaece7ba0
James Cook <james.cook@usma.edu>
2011-06-16 15:25:53 +0000

    Removed MATREX headers and made rti.h check for sanity of API choice flags

--- include/wrap/rti.h
+++ include/wrap/rti.h
@@ -1,17 +1,22 @@
 #ifndef ___n_rti_h__
 #define ___n_rti_h__
 
-#ifdef RTI_NG_MATREX_API
+#ifdef RTI_NG_API
+#   ifdef DLC_API
+#       error RTI_NG_API and DLC_API are both set!  That's just silly!
+#   endif
 #   include "RTI.hh"
 #   include "NullFederateAmbassador.hh"
 #   include "fedtime.hh"
 #   define rti13 RTI
-#   define NullFedAmb NullFederateAmbassador
+#   define NullFedAmb 
 #elif defined(DLC_API)
 #   include "RTI13.h"
 #   include "NullFederateAmbassador13.h"
 #   include "fedtime13.h"
 #   define NullFedAmb rti13::NullFederateAmbassador
+#else
+#   error RTI interface version macro not set (#define either RTI_NG_API or DLC_API)
 #endif
 
 #endif /* ___n_rti_h__ */
